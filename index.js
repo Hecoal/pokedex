4cad7a0cc5531a45cc0a1845889379f95d6d2430
Hecoal <hectorcortez_2002@hotmail.com>
2023-09-10 00:54:10 -0600

    modified indexjs

--- index.js
+++ index.js
@@ -9,14 +9,38 @@ app.get('/', (req,res,next)=>{
     res.status(200); //everything is fine
     res.send('Welcome to Pokedex');
 });
-app.get('/:pokemon', (req,res,next)=>{
+
+//We import the pokemon with the same id as the URL. Making sure it only accepts numbers with REGEX
+app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
+    //We set the limits to search a pokemon from the json
+    const id=req.params.id-1;
+    if(id>=0 && id<=151){
+        res.status(200);
+        res.send(pokemon[req.params.id -1]);
+    }else{
+        res.status(404);
+        res.send('Pokemon not found');
+    }
+    
+});
+app.get('/:pokemon/all', (req,res,next)=>{
     res.status(200);
     res.send(pokemon);
 });
 
-app.get('/pokemon/:id',(req,res,next)=>{
-    res.status(200);
-    res.send(pokemon);
+//Route to find pokemon by their names
+app.get('/pokemon/:name',(req,res,next)=>{
+    //We get the name from url
+    const name= req.params.name;
+    for(i=0;i<pokemon.length;i++){
+        //Pokemon in the i position, lets get the name
+        if(pokemon[i].name==name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send('Pokemon not found');
 });
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Server is running...');

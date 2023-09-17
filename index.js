c4a6246519b0938effa55e06bb4a9487c1b3692c
Hecoal <hectorcortez_2002@hotmail.com>
2023-09-16 20:41:34 -0600

    Added ternary if

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-//Import data base
+//Import data base, save it on an array
 const { pokemon }=require('./pokedex.json');
 
-//This function doesnt have a name, and opens a port. Is the same as function(){}
 //Verbs HTTP (to make petitions): GET, POST (save data), PATCH (update a single data), PUT (update all the data), DELETE (delete)
 app.get('/', (req,res,next)=>{
-    res.status(200); //everything is fine
-    res.send('Welcome to Pokedex');
+    
+    return res.status(200).send('Welcome to Pokedex');
+    //Alternative res.status(200)
+    //            res.send('Welcome to pokedex')
+});
+
+//Return all the data when you type the following URL:
+app.get('/:pokemon', (req,res,next)=>{
+    return res.status(200).send(pokemon);
 });
 
 //We import the pokemon with the same id as the URL. Making sure it only accepts numbers with REGEX
@@ -15,33 +21,47 @@ app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
     //We set the limits to search a pokemon from the json
     const id=req.params.id-1;
     if(id>=0 && id<=151){
-        res.status(200);
-        res.send(pokemon[req.params.id -1]);
+        return res.status(200).send(pokemon[req.params.id -1]);
     }else{
-        res.status(404);
-        res.send('Pokemon not found');
+        return res.status(404).send('Pokemon not found');
     }
     
 });
-app.get('/:pokemon/all', (req,res,next)=>{
-    res.status(200);
-    res.send(pokemon);
+
+//Route to find pokemon by their names. Making sure it only accpets letters with REGEX
+app.get('/pokemon/:name([A-Za-z]+)',(req,res,next)=>{
+    const name = req.params.name;
+
+    //We filter the entire array
+    const pk = pokemon.filter((p)=>{
+
+        //We look for the URL name and the JSON
+
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+
+        // if(p.name.toUpperCase() == name.toUpperCase()){
+        //     return p;
+        // }
+    });
+    //(condition ? valor if its true : value if its false)
+    (pk.length>0) ? res.status(200).send(pk): res.status(404).send('Pokemon not found');
+    
+    /*This is the same as doing....
+
+        //We get the name from url
+        const name= req.params.name;
+        for(i=0;i<pokemon.length;i++){
+            //Pokemon in the i position, lets get the name
+            if(pokemon[i].name.toLocaleUpperCase()==name.toLocaleUpperCase()){
+                return res.status(200).send(pokemon[i]);
+            }
+        }
+        return res.status(404).send('Pokemon not found')*/
+
+
 });
 
-//Route to find pokemon by their names
-app.get('/pokemon/:name',(req,res,next)=>{
-    //We get the name from url
-    const name= req.params.name;
-    for(i=0;i<pokemon.length;i++){
-        //Pokemon in the i position, lets get the name
-        if(pokemon[i].name==name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send('Pokemon not found');
-});
+//This function doesnt have a name, and opens a port. Is the same as function(){}
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Server is running...');
 });

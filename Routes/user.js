f65d1ee30a22cc91cc602831395faebc055289c2
Hecoal <hectorcortez_2002@hotmail.com>
2023-10-02 20:38:47 -0600

    Added json web token

--- Routes/user.js
+++ Routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../Config/database');
 
@@ -6,7 +7,7 @@ user.post('/',async(req,res,next)=>{
     const {user_name, user_mail, user_password}=req.body;
     if(user_name && user_mail && user_password){
         let query = 'INSERT INTO user( user_name,user_mail, user_password) ';
-        query+=`VALUES ('${user_name}','${user_mail}', '${user_password}')`;
+        query+=`VALUES ('${user_name}','${user_mail}', '${user_password}');`;
 
         const rows = await db.query(query);
         if(rows.affectedRows==1){
@@ -18,6 +19,27 @@ user.post('/',async(req,res,next)=>{
     
 });
 
+user.post('/login', async (req, res, next)=>{
+    const {user_mail,user_password}=req.body;
+    const query= `SELECT * FROM user WHERE user_mail='${user_mail}' AND user_password = '${user_password}';`;
+    const rows=await db.query(query);
+
+    if(user_mail && user_password){
+        if(rows.length ==1){
+            const token = jwt.sign({
+                user_id:rows[0].user_id,
+                user_mail: rows[0].user_mail
+            },'debugkey');
+            return res.status(200).json({code:200, message:token});
+    
+        }else{
+            return res.status(401).json({code:401, message:'User / password incorrect'})
+        }
+    }
+    return res.status(500).json({code:500, message:'Empty fields'})
+    
+    
+});
 user.get('/',async (req,res,next)=>{
     const query = 'SELECT * FROM user';
     const rows = await db.query(query);
